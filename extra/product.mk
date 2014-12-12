959cf60587dc5b479429e54f845a5e65762abc57
tuxafgmur <usuario@localhost.localdomain>
2014-12-12 02:11:07 +0100

    Set wallpapers by device

--- extra/product.mk
+++ extra/product.mk
@@ -16,47 +16,7 @@ PRODUCT_COPY_FILES += \
 
 # Wallpaper
 PRODUCT_COPY_FILES += \
-	vendor/extra/wallpaper/WallpaperPicker.apk:system/app/WallpaperPicker.apk \
-	vendor/extra/wallpaper/wallpapers/wallpaper_01.jpg:system/media/wallpapers/wallpaper_01.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_01_small.jpg:system/media/wallpapers/wallpaper_01_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_02.jpg:system/media/wallpapers/wallpaper_02.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_02_small.jpg:system/media/wallpapers/wallpaper_02_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_03.jpg:system/media/wallpapers/wallpaper_03.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_03_small.jpg:system/media/wallpapers/wallpaper_03_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_04.jpg:system/media/wallpapers/wallpaper_04.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_04_small.jpg:system/media/wallpapers/wallpaper_04_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_05.jpg:system/media/wallpapers/wallpaper_05.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_05_small.jpg:system/media/wallpapers/wallpaper_05_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_06.jpg:system/media/wallpapers/wallpaper_06.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_06_small.jpg:system/media/wallpapers/wallpaper_06_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_07.jpg:system/media/wallpapers/wallpaper_07.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_07_small.jpg:system/media/wallpapers/wallpaper_07_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_08.jpg:system/media/wallpapers/wallpaper_08.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_08_small.jpg:system/media/wallpapers/wallpaper_08_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_09.jpg:system/media/wallpapers/wallpaper_09.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_09_small.jpg:system/media/wallpapers/wallpaper_09_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_10.jpg:system/media/wallpapers/wallpaper_10.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_10_small.jpg:system/media/wallpapers/wallpaper_10_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_11.jpg:system/media/wallpapers/wallpaper_11.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_11_small.jpg:system/media/wallpapers/wallpaper_11_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_12.jpg:system/media/wallpapers/wallpaper_12.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_12_small.jpg:system/media/wallpapers/wallpaper_12_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_13.jpg:system/media/wallpapers/wallpaper_13.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_13_small.jpg:system/media/wallpapers/wallpaper_13_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_14.jpg:system/media/wallpapers/wallpaper_14.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_14_small.jpg:system/media/wallpapers/wallpaper_14_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_15.jpg:system/media/wallpapers/wallpaper_15.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_15_small.jpg:system/media/wallpapers/wallpaper_15_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_16.jpg:system/media/wallpapers/wallpaper_16.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_16_small.jpg:system/media/wallpapers/wallpaper_16_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_17.jpg:system/media/wallpapers/wallpaper_17.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_17_small.jpg:system/media/wallpapers/wallpaper_17_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_18.jpg:system/media/wallpapers/wallpaper_18.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_18_small.jpg:system/media/wallpapers/wallpaper_18_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_19.jpg:system/media/wallpapers/wallpaper_19.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_19_small.jpg:system/media/wallpapers/wallpaper_19_small.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_20.jpg:system/media/wallpapers/wallpaper_20.jpg \
-	vendor/extra/wallpaper/wallpapers/wallpaper_20_small.jpg:system/media/wallpapers/wallpaper_20_small.jpg
+	vendor/extra/wallpaper/WallpaperPicker.apk:system/app/WallpaperPicker.apk
 
 # Xbin
 PRODUCT_COPY_FILES += \
