d342d784445e248ada9fa6d9e74324028359a729
tuxafgmur <usuario@localhost.localdomain>
2015-01-03 04:09:34 +0100

    Clean final build a bit

--- samsung/p51xx/p51xx-vendor-blobs.mk
+++ samsung/p51xx/p51xx-vendor-blobs.mk
@@ -15,7 +15,6 @@
 LOCAL_PATH := vendor/samsung/p51xx
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/common/system/bin/dock_kbd_attach:system/bin/dock_kbd_attach \
     $(LOCAL_PATH)/common/system/bin/geomagneticd:system/bin/geomagneticd \
     $(LOCAL_PATH)/common/system/bin/gpsd:system/bin/gpsd \
     $(LOCAL_PATH)/common/system/bin/hostapd:system/bin/hostapd \
